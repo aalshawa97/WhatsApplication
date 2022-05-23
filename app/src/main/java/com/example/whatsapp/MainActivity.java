6d4434aaf43fb7da86693d660b3914413d748012
aalshawa97 <abdullahalshawa@gmail.com>
2022-05-23 16:55:46 -0700

    Main activity
    Phone verification

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mPhoneNumber, Code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
